e44bee3f6ab61dd5c2ea0ffec1177ec06f12907f
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-03-06 14:34:28 -0300

    feat: including changes to pass tests

--- src/pages/home/components/Header/index.tsx
+++ src/pages/home/components/Header/index.tsx
@@ -1,12 +1,30 @@
+import { Button, Flex } from "@chakra-ui/react";
+import Image from "next/image";
+import Logo from "@/assets/img/logo.png";
+import React from "react";
+
 export function Header() {
+  const [windowWidth, setWindowWidth] = React.useState<number>(0);
+  const [isMobileNavOpened, setIsMobileNavOpened] = React.useState(false);
+
+  React.useEffect(() => setWindowWidth(window.innerWidth), []);
+
   return (
-    <nav>
-      <ul>
-        <li><a href="#">A experiência</a></li>
-        <li><a href="#">Mapa de Setores</a></li>
-        <li><a href="#">Informações</a></li>
-        <li><a href="#">Ingresso</a></li>
-      </ul>
-    </nav>
+    <>
+      <Flex
+        as="header"
+        justifyContent="space-between"
+        bgColor="brand.primary.400"
+      >
+        <Image src={Logo} alt="logo" width={234} height={64} />
+        {windowWidth < 768 && (
+          <Button onClick={() => setIsMobileNavOpened(actualState => !actualState)}>
+            <Image src="/menu.png" alt="navegação" width={32} height={32} />
+          </Button>
+        )}
+      </Flex>
+      
+      {isMobileNavOpened && <nav></nav>}
+    </>
   );
 }
