631a86860d9fa459bbb439d9b05e8033cee42a97
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-03-17 10:47:01 -0300

    feat: new breakpoints

--- src/assets/theme/index.ts
+++ src/assets/theme/index.ts
@@ -3,16 +3,20 @@ import { extendBaseTheme, extendTheme } from "@chakra-ui/react";
 const colors = {
   brand: {
     primary: {
-      200: '#C8DEEF',
-      400: '#2E7BA2',
+      200: "#C8DEEF",
+      400: "#2E7BA2",
     },
     secondary: {
-      200: '#FBF0DD',
-      300: '#FACF9D',
-      400: '#DF9010',
-    }
+      200: "#FBF0DD",
+      300: "#FACF9D",
+      400: "#DF9010",
+    },
+    breakpoints: {
+      sm: "22,5rem", //360px
+      md: "48rem", //768px
+      lg: "90rem" //1440px
+    },
   },
 };
 
 export const theme = extendTheme({ colors });
-
--- src/pages/home/components/Header/index.tsx
+++ src/pages/home/components/Header/index.tsx
@@ -20,7 +20,7 @@ export default function Header() {
         justifyContent="space-between"
         alignItems="center"
         bgColor="brand.primary.400"
-        padding={["2rem 1.5rem", "2rem 3.75rem", null, "2.75rem 7.5rem"]}
+        padding={{sm: '2rem 1.5rem', md: '2rem 3.75rem', lg: '2.75rem 7.5rem'}}
       >
         <Image src={Logo} alt="logo" width={234} height={64} />
         {windowWidth < 768 ? (
