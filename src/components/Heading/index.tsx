3998bd8d1f6c12f4513fc22ba2771ce14173adf7
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-03-20 15:34:23 -0300

    fix: changing extended type

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,15 +1,15 @@
-import { Box } from "@chakra-ui/react";
-import { HTMLAttributes, ReactNode } from "react";
+import { Box, TypographyProps } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
-interface Props extends HTMLAttributes<HTMLOrSVGElement> {
+interface Props extends TypographyProps {
   as?: 'h1'| 'h2' | 'h3' | 'h4' | 'h5' | 'h6',
   size: 'md' | 'lg'| 'xl'
   children: ReactNode
 }
 
-export function Heading({as = 'h2', size = 'md' ,children}: Props) {
+export function Heading({as = 'h2', size = 'md' , children, ...rest}: Props) {
   return (
-    <Box as={as} textStyle={`heading.${size}`} >
+    <Box as={as} textStyle={`heading.${size}`} {...rest} textAlign="center" >
       {children}
     </Box>
   );
--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,14 +1,14 @@
-import { Box } from "@chakra-ui/react";
+import { Box, TypographyProps } from "@chakra-ui/react";
 import { HTMLAttributes } from "react";
 
-interface Props extends HTMLAttributes<HTMLOrSVGElement> {
+interface Props extends TypographyProps {
   as?: 'p' | 'span' | 'a',
   children: string
 }
 
-export function Text({as, children}: Props) {
+export function Text({as, children, ...rest}: Props) {
   return(
-    <Box as={as} textStyle="text">
+    <Box as={as} textStyle="text" {...rest}>
       {children}
     </Box>
   );
